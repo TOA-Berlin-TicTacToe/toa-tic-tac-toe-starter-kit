4fe74e226b92ae9f04e99b3e62b9f18a44099155
LFDM <1986gh@gmail.com>
2017-07-13 17:41:47 +0200

    refactor(with-tic-tac-toe) Remove duplication

--- src/withTicTacToe/index.js
+++ src/withTicTacToe/index.js
@@ -54,29 +54,21 @@ const mapEmit = (emit, { game, games, username, isInGame, hasPendingGame }) => (
   }
 });
 
+const tryAiMove = (props, game) => {
+    const { ai, username } = props;
+    if (ai && ai.move && username === game.turn) {
+      // we could be nice and benchmark here, so that we immediately
+      // report back to people how long their AI takes to make a move ;)
+      const nextPosition = ai.move(game);
+      if (nextPosition) {
+        props.actions.move(nextPosition);
+      }
+    }
+}
+
 const callbacks = () => ({
-  'client:move': (props, game) => {
-    const { ai, username } = props;
-    if (ai && ai.move && username === game.turn) {
-      // we could be nice and benchmark here, so that we immediately
-      // report back to people how long their AI takes to make a move ;)
-      const nextPosition = ai.move(game);
-      if (nextPosition) {
-        props.actions.move(nextPosition);
-      }
-    }
-  },
-  'client:init': (props, game) => {
-    const { ai, username } = props;
-    if (ai && ai.move && username === game.turn) {
-      // we could be nice and benchmark here, so that we immediately
-      // report back to people how long their AI takes to make a move ;)
-      const nextPosition = ai.move(game);
-      if (nextPosition) {
-        props.actions.move(nextPosition);
-      }
-    }
-  }
+  'client:move': tryAiMove,
+  'client:init': tryAiMove
 });
 
 export default withSocket({ initialState, mapData, mapEmit, callbacks });
